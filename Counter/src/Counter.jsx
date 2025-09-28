25e8b8fe311845050b04910481d3d0d5b76bc6da
Jake M <jakemanson77@icloud.com>
2025-09-28 18:28:25 +0100

    feat(counter): add increment, decrement, and reset functionality; remove unused template CSS

---
+++ Counter/src/Counter.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+
+    return (
+        <div>
+
+            <p className="displayCounter">{count}</p>
+
+            <button className="counterButtons" onClick={decrement}>Decrement</button>
+            <button className="counterButtons" onClick={reset}>Reset</button>
+            <button className="counterButtons" onClick={increment}>Increment</button>
+
+
+
+        </div>
+    )
+
+}
+
+export default Counter
